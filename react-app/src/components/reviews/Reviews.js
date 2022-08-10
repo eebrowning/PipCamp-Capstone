e08491884a6b0b88e2ee3aded0650dc1becb09c5
eebrowning <eebrowning828@gmail.com>
2022-08-10 16:06:20 -0600

    about ready for pregrade!

--- react-app/src/components/reviews/Reviews.js
+++ react-app/src/components/reviews/Reviews.js
@@ -12,6 +12,14 @@ function Reviews({ locationId }) {
     const locationReviews = reviews.filter(review => review.location_id === +locationId)
     // const [reviewed, setReviewed] = useState(false)
     const [editing, setEditing] = useState(false)
+    // const { locationId } = useParams()
+    const locations = useSelector(state => Object.values(state.locations))
+    const location = locations?.find(location => location.id == +locationId)
+    const [usersArr, setUsersArr] = useState([]);
+    console.log('>>>>>>>>>>>>>> usersArr', usersArr)
+    const matchUser = (userArr, id) => {//swap user_id for username
+        return userArr?.find(user => user.id === id)?.username
+    }
     let userReview;
     locationReviews.forEach(review => {
         if (user && review.user_id == user.id) {
@@ -21,6 +29,12 @@ function Reviews({ locationId }) {
 
 
     useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/api/users/');
+            const responseData = await response.json();
+            setUsersArr(responseData.users);
+        }
+        fetchData();
         console.log('dispatched to GetReviewsThunk')
         dispatch(GetReviewsThunk())
     }, [dispatch])
@@ -38,15 +52,22 @@ function Reviews({ locationId }) {
             {/* add-review button goes here, will be conditionally available if no reviews by user*/}
             {!userReview && (<ReviewForm locationId={locationId} />)}
             {locationReviews && locationReviews.map(review => (
-                <div className='review-box'>
+                <div key={review.id} className='review-box'>
 
                     <div className='review-content' key={review.id}>
                         <div className='user-card'>
-                            <img src="https://www.pngmart.com/files/15/Fallout-Pip-Boy-PNG-Transparent.png" className='user-image' />
+                            {review.recommends && (
+                                <img src="https://i.imgur.com/rUN6XwH.png" className='user-image' />
+
+                            )}
+                            {!review.recommends && (
+                                <img src="https://i.imgur.com/KrC8Fj5.png" className='user-image' />
+                            )}
+
                         </div>
                         <div className='review-card'>
                             <div className='recommendation'>
-                                <div>User {review.user_id}</div>
+                                <div className='user-name'>{matchUser(usersArr, review.user_id)}</div>
                                 <div>{review.recommends ? ' recommends this listing.' : ' does not recommend this listing.'}</div>
                             </div>
                             <div>{review.content}</div>
